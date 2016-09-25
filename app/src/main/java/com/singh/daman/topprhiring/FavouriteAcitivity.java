81634f7d9472890e0a46bcd4522b9181dc7d8870
Damanpreet Singh <singhdaman4321@gmail.com>
2016-09-25 15:05:01 +0530

    fragment

--- app/src/main/java/com/singh/daman/topprhiring/FavouriteAcitivity.java
+++ app/src/main/java/com/singh/daman/topprhiring/FavouriteAcitivity.java
@@ -2,6 +2,8 @@ package com.singh.daman.topprhiring;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class FavouriteAcitivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class FavouriteAcitivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.containerfav, new FavouriteFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+//        if (id == R.id.action_settings) {
+//            startActivity(new Intent(this, SettingsActivity.class));
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/singh/daman/topprhiring/FavouriteFragment.java
@@ -0,0 +1,9 @@
+package com.singh.daman.topprhiring;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by daman on 25/9/16.
+ */
+public class FavouriteFragment extends Fragment {
+}
